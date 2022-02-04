45170141bc0fa51c20f04ac76a83667b1c18a5bc
danielp345 <danielp345@gmail.com>
2022-02-04 23:19:20 +0100

    deleting feedbacks and showing avg and num of feedbacks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import Header from './components/Header'
-import FeedbackItem from './components/FeedbackItem'
+import { useState } from "react"
+import Header from "./components/Header"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
 
-const App = () => {
+function App() {
+	const [feedback, setFeedback] = useState(FeedbackData)
+
+	const deleteFeedback = (id) => {
+		if (window.confirm("Are you sure you want to delete?")) {
+			setFeedback(feedback.filter((item) => item.id !== id))
+		}
+	}
+
 	return (
-        <>
-        <Header />
-        <div className="container">
-            <FeedbackItem />
-        </div>
-        </>
-    )
+		<>
+			<Header />
+			<div className="container">
+				<FeedbackStats feedback={feedback} />
+				<FeedbackList feedback={feedback} hendleDelete={deleteFeedback} />
+			</div>
+		</>
+	)
 }
 
 export default App
