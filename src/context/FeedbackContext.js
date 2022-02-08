2214e05536258015df0a9ed46e78d823d0cdc63d
danielp345 <danielp345@gmail.com>
2022-02-08 23:11:51 +0100

    add updateFeedback function

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,23 +22,50 @@ export const FeedbackProvider = ({ children }) => {
 		},
 	])
 
+	const [feedbackEdit, setFeedbackEdit] = useState({
+		item: {},
+		edit: false,
+	})
+
+	// Add Feedback
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4()
 		setFeedback([newFeedback, ...feedback])
 	}
 
+	// Delete Feedback
 	const deleteFeedback = (id) => {
 		if (window.confirm("Are you sure you want to delete?")) {
 			setFeedback(feedback.filter((item) => item.id !== id))
 		}
 	}
 
+	// { ...item, ...updItem} scala obiekty naspisując powtarzające się klucze wartościami obiektu drugiego z kolei
+
+	// Update feedback item
+	const updateFeedback = (id, updItem) => {
+		setFeedback(
+			feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+		)
+	}
+
+	// Set item to be updated
+	const editFeedback = (item) => {
+		setFeedbackEdit({
+			item,
+			edit: true,
+		})
+	}
+
 	return (
 		<FeedbackContext.Provider
 			value={{
 				feedback,
+				feedbackEdit,
 				addFeedback,
 				deleteFeedback,
+				editFeedback,
+				updateFeedback,
 			}}
 		>
 			{children}
